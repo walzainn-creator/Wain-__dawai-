9290cd00151e333f82fdbf7f28d8b97ddb63ee82
We elzain <walzainn@gmail.com>
2026-08-31 05:53:13 +0400

    Update supabaseUrl to remove protocol

--- app-config.js
+++ app-config.js
@@ -9,7 +9,7 @@ window.WINDAWAI_CONFIG = {
   projectNameArabic: "وين دواي؟",
   owner: "Arfa Al-Jaili",
   designer: "Wail Elzain",
-  supabaseUrl: "https://supabase.com/dashboard/org/ixgcxebqbqtysqvonuqp",
+  supabaseUrl: "supabase.com/dashboard/org/ixgcxebqbqtysqvonuqp",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0ZHp5aXB0Zm56dHd5bXh0a2p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxMjUwNTYsImV4cCI6MjEwMzcwMTA1Nn0.bi7j3xf0zCX1pNo1Zam0_HCqEq_1Ex5UwP2kIZgdGeI",
   defaultCountry: "Sudan",
   currency: "SDG"
