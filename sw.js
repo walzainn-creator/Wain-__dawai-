b3c662f0ea9f8c0cd533671370f0a8e1c9fb9071
We elzain <walzainn@gmail.com>
2026-08-31 05:14:58 +0400

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE="windawai-shell-v2";
-const ASSETS=["./","./index.html","./manifest.json","./app-config.js","./404.html"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match("./index.html"))));
-});
